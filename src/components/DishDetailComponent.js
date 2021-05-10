5a7c42f8c410a53e6243864ff580c6b16777b528
Adarsh <adarshraj0210@gmail.com>
2021-05-10 21:06:42 +0530

    Presentational and Container Component

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -10,13 +10,13 @@ class DishDetail extends Component{
     renderComments(comments)
     {
         if (comments != null) {
-            let options = { year: "numeric", month: "short", day: "numeric" };
+            let options = { year: "numeric", month: "short", day: "2-digit" };
             return comments.map(comment => (
-              <ul key={comment.id} className="list-unstyled">
+                <ul key={comment.id} className="list-unstyled">
                 <li className="mb-2">{comment.comment}</li>
                 <li>
                   -- {comment.author}{" "}
-                  {new Date(comment.date).toLocaleDateString("en-US", options)}
+                  {new Intl.DateTimeFormat("en-US", options).format(new Date(Date.parse(comment.date)))}
                 </li>
               </ul>
             ));
@@ -28,20 +28,23 @@ class DishDetail extends Component{
     {
         if( dish!=null )
         return(
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={dish.image} alt={dish.name} />
+                            <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                            <h4>Comments</h4>
+                            {this.renderComments(dish.comments)}
+                    </div>
                 </div>
-                <div className="col-12 col-md-5 m-1">
-                <h4>Comments</h4>
-                {this.renderComments(dish.comments)}
-                </div>
+                
             </div>
         );
         return(
