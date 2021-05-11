11eaff4127c3f6b1be7c7fcbd3d8542c6e713da0
Adarsh <adarshraj0210@gmail.com>
2021-05-11 11:13:33 +0530

    Functional Component

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,61 +1,53 @@
-import { Component } from "react";
+import React from 'react';
 import {Card, CardImg,CardText, CardBody,CardTitle } from 'reactstrap';
 
-class DishDetail extends Component{
-
-    constructor(props) {
-        super(props);
+function RenderComments({comments})
+{
+    if (comments != null) {
+        let options = { year: "numeric", month: "short", day: "2-digit" };
+        return comments.map(comment => (
+            <ul key={comment.id} className="list-unstyled">
+            <li className="mb-2">{comment.comment}</li>
+            <li>
+                -- {comment.author}{" "}
+                {new Intl.DateTimeFormat("en-US", options).format(new Date(Date.parse(comment.date)))}
+            </li>
+            </ul>
+        ));
     }
+    return <div></div>;
+}
 
-    renderComments(comments)
-    {
-        if (comments != null) {
-            let options = { year: "numeric", month: "short", day: "2-digit" };
-            return comments.map(comment => (
-                <ul key={comment.id} className="list-unstyled">
-                <li className="mb-2">{comment.comment}</li>
-                <li>
-                  -- {comment.author}{" "}
-                  {new Intl.DateTimeFormat("en-US", options).format(new Date(Date.parse(comment.date)))}
-                </li>
-              </ul>
-            ));
-        }
-        return <div></div>;
-    }
+function RenderDish( {dish} )
+{
+    return(
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+    );
+}
+const DishDetail = (props) => {
 
-    renderDish(dish)
-    {
-        if( dish!=null )
-        return(
+    if(props.dish != null) {
+        return (
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg top src={dish.image} alt={dish.name} />
-                            <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
+                        <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                            <h4>Comments</h4>
-                            {this.renderComments(dish.comments)}
+                        <h4>Comments</h4>
+                        <RenderComments comments={props.dish.comments} />
                     </div>
                 </div>
-                
             </div>
         );
-        return(
-             <div></div>
-        );
-
     }
-    render() {
-        const dish = this.props.dish;
-        return (this.renderDish(dish));
-    }
-
+    return <div></div> ;
 }
+
 export default DishDetail;
