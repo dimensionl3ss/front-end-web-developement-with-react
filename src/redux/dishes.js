019c55432aa4d2bc330cc79a0c4f27cbec413c65
Adarsh <adarshraj0210@gmail.com>
2021-05-19 13:17:01 +0530

    Combining Reducers

---
+++ src/redux/dishes.js
@@ -0,0 +1,8 @@
+import { DISHES } from '../shared/dishes';
+
+export const Dishes = (state = DISHES, action) => { //simpler reducer function
+    switch (action.type) {
+        default:
+          return state;
+      }
+};
