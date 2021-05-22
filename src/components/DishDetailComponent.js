6bb31d528d1b1363b07624f5755db27b8fe13896
Adarsh <adarshraj0210@gmail.com>
2021-05-22 20:38:07 +0530

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom';
 
 import { Component } from 'react'
 import { Control, LocalForm, Errors} from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -150,7 +151,25 @@ function RenderDish( {dish} )
 }
 const DishDetail = (props) => {
 
-    if(props.dish != null) {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if(props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
